08d2ace3de4957ee37b134c9a7e849cb97ca5752
Sascha Metz <metz@raum3.de>
2019-04-29 16:53:00 +0200

    Adding main-component with initial static html

---
+++ src/components/main.js
@@ -0,0 +1,34 @@
+import React from "react"
+
+/** Component */
+const Main = () => (
+  <main>
+    <h2>Herzlich Willkommen</h2>
+    <p>
+      Wir unterstützen Sie bei Projekten sowie Migrationen, erarbeiten
+      ganzheitlich Betriebskonzepte und stellen Ihnen zertifizierte Partner zur
+      Verfügung. Selbstverständlich kümmern wir uns auch um die Absicherung der
+      installierten Systeme.
+    </p>
+    <p>
+      Gemeinsam mit Ihnen ermitteln wir Ihre Sicherheitsanforderungen und setzen
+      diese um. Dabei achten wir immer auf den Kosten–Nutzen–Usability-Faktor
+      zwischen Sicherheit und produktivem Arbeiten sowie der Wirtschaftlichkeit
+      von IT-Sicherheit.
+    </p>
+    <p>
+      Für Ihre Mitarbeiter stellen wir bei Bedarf Lösungen für den Fernzugriff
+      auf Ihre Daten, E-Mails und Anwendungen zur Verfügung. Wir entwickeln
+      gemäß Ihren Vorgaben individuelle Softwareanwendungen, die von einer
+      Standard-Software nicht abgedeckt werden.
+    </p>
+    <p>
+      Unser Ziel ist es, die Geschäfts- Prozesse und deren Abläufe unserer
+      Kunden durch moderne, bedarfsgerechte IT-Lösungen und ein umfangreiches
+      Service-Angebot zu unterstützen und für unseren Kunden gewinnbringend zu
+      optimieren.
+    </p>
+  </main>
+)
+
+export default Main
