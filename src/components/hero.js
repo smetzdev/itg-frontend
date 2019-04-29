d1ab858b26ba806280613f7400a9e2df53c2106d
Sascha Metz <metz@raum3.de>
2019-04-29 16:48:05 +0200

    adding hero.js component with inital html

---
+++ src/components/hero.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Frank from "../images/passfoto_cutted.png"
+
+/** Component */
+const Hero = () => (
+  <section>
+    <h1>IT-Consulting Grossmann</h1>
+    <img src={Frank} alt="Frank Grossmann" />
+  </section>
+)
+
+export default Hero
