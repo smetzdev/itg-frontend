d303e487044e89b61b359b186d395cba8023bde7
Sascha Metz <metz@raum3.de>
2019-04-29 19:00:45 +0200

    Adding GlobalStyle to layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,16 +7,17 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-
 import Header from "./header"
 import Footer from "./footer"
+import { GlobalStyle } from "../elements"
 
 /** Component */
 const Layout = ({ children }) => (
   <>
+    <GlobalStyle />
     <Header />
     {children}
-    <Footer/>
+    <Footer />
   </>
 )
 
