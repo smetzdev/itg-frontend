c3db9eb887d7a888ea736c04ac376734071c9a98
Sascha Metz <metz@raum3.de>
2019-04-30 21:45:18 +0200

    Adding screenSizes to utilities

--- src/utilities/index.js
+++ src/utilities/index.js
@@ -1,3 +1,4 @@
 export * from "./colors"
 export * from "./typo"
 export * from "./spacing"
+export * from "./screenSizes"
---
+++ src/utilities/screenSizes.js
@@ -0,0 +1,6 @@
+const screenSizes = {
+  small: 720,
+  medium: 980,
+  big: 1200,
+  huge: 1400,
+}
