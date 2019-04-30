8ac9f9fd3eb0d01df96b6b6a1fd5577e24935e5e
Sascha Metz <metz@raum3.de>
2019-04-30 22:46:25 +0200

    Responsive Footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,7 +2,7 @@ import React from "react"
 import styled from "styled-components"
 import { Link } from "gatsby"
 import { FlexContainer } from "../elements"
-import { colors, typo } from "../utilities"
+import { colors, typo, spacing, screenSizes } from "../utilities"
 
 /** Component */
 const Footer = () => {
@@ -29,17 +29,24 @@ const StyledFooter = styled.footer`
   background-color: ${colors.primary.darker};
   color: ${colors.primary.lightest};
   ${FlexContainer} {
-    height: 100px;
+    flex-wrap: wrap;
+    justify-content: center;
+    padding-top: ${spacing.half / 2}px;
+    padding-bottom: ${spacing.half / 2}px;
   }
   h4,
   ul {
     font-size: ${typo.text.footer.size}px;
     text-transform: uppercase;
+    padding: 0;
+    margin: 0;
   }
   ul {
+    margin-top: 10px;
     list-style-type: none;
     li {
       display: inline-block;
+      padding: 0;
       &:not(:last-child) {
         margin-right: 32px;
       }
@@ -47,6 +54,23 @@ const StyledFooter = styled.footer`
         color: inherit;
         text-decoration: none;
       }
+    }
+  }
+  @media screen and (min-width: ${screenSizes.small - 114}px) {
+    ul {
+      margin-top: 0;
+      margin-left: 30px;
+    }
+  }
+  @media screen and (min-width: ${screenSizes.medium}px) {
+    ${FlexContainer} {
+      height: 100px;
+      padding-top: 0;
+      padding-bottom: 0;
+      justify-content: space-between;
+    }
+    ul {
+      margin-left: 0;
     }
   }
 `
