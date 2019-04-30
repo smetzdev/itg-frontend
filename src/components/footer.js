441ea0643cfc4e2151374b9d7302efc48be94e5f
Sascha Metz <metz@raum3.de>
2019-04-30 18:15:50 +0200

    adding initial styling to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,21 +1,54 @@
 import React from "react"
+import styled from "styled-components"
 import { Link } from "gatsby"
+import { FlexContainer } from "../elements"
+import { colors, typo } from "../utilities"
 
+/** Component */
 const Footer = () => {
   const currentYear = new Date().getFullYear()
   return (
-    <footer>
-      <h4>&copy; {currentYear} IT-Consulting Grossmann</h4>
-      <ul>
-        <li>
-          <Link to="/impressum">Impressum</Link>
-        </li>
-        <li>
-          <Link to="/datenschutz">Datenschutz</Link>
-        </li>
-      </ul>
-    </footer>
+    <StyledFooter>
+      <FlexContainer>
+        <h4>&copy; {currentYear} IT-Consulting Grossmann</h4>
+        <ul>
+          <li>
+            <Link to="/impressum">Impressum</Link>
+          </li>
+          <li>
+            <Link to="/datenschutz">Datenschutz</Link>
+          </li>
+        </ul>
+      </FlexContainer>
+    </StyledFooter>
   )
 }
+
+/** Styling */
+const StyledFooter = styled.footer`
+  background-color: ${colors.primary.darker};
+  color: ${colors.primary.lightest};
+  ${FlexContainer} {
+    height: 100px;
+  }
+  h4,
+  ul {
+    font-size: ${typo.text.footer.size}px;
+    text-transform: uppercase;
+  }
+  ul {
+    list-style-type: none;
+    li {
+      display: inline-block;
+      &:not(:last-child) {
+        margin-right: 32px;
+      }
+      a {
+        color: inherit;
+        text-decoration: none;
+      }
+    }
+  }
+`
 
 export default Footer
