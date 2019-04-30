a14f479823fd0b03a99ed2f7a38f833978ef99b9
Sascha Metz <metz@raum3.de>
2019-04-30 18:26:27 +0200

    Adding global font settings

--- src/elements/globalStyle.js
+++ src/elements/globalStyle.js
@@ -7,8 +7,15 @@ export const GlobalStyle = createGlobalStyle`
   }
   
   html,body {
+    font-family: 'Roboto', Arial, Helvetica, sans-serif;
+    font-weight: 400;
     padding: 0;
     margin: 0;
     font-size: ${typo.text.std.size}px;
+  }
+  h1,h2,h3,h4{
+    font-weight: 700;
+    margin-top: 0;
+    margin-bottom: 10px;
   }
 `
