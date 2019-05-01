59d9f5251f3484d4dc27d4a21d23869bd0712efb
Sascha Metz <metz@raum3.de>
2019-05-01 20:01:04 +0200

    Adding elements/textPage component

---
+++ src/elements/textPage.js
@@ -0,0 +1,39 @@
+import React from "react"
+import styled from "styled-components"
+import { spacing, typo, colors } from "../utilities"
+import { Container } from "./container"
+import CircuitBG from "../images/circuit-board.svg"
+
+/** Component */
+const TextPage = ({ children }) => (
+  <StyledTextPage>
+    <Container>{children}</Container>
+  </StyledTextPage>
+)
+
+/** Styling */
+const StyledTextPage = styled.main`
+  padding-top: ${spacing.half}px;
+  padding-bottom: ${spacing.half}px;
+  background-image: url(${CircuitBG});
+  h1,
+  h2 {
+    text-transform: ${typo.headlines.medium.transform};
+    color: ${colors.grey.darker};
+    word-break: break-all;
+    hyphens: auto;
+  }
+  h3,
+  h4,
+  h5,
+  h6 {
+    text-transform: uppercase;
+    color: ${colors.grey.darker};
+    margin-bottom: 2px !important;
+  }
+  p {
+    margin-top: 0;
+  }
+`
+
+export default TextPage
