679df2f3db002c5de5d73e40daa76843720c8335
Sascha Metz <metz@raum3.de>
2019-05-07 21:01:57 +0200

    Adding StaticQuery to index/main.js

--- src/components/index/main.js
+++ src/components/index/main.js
@@ -1,43 +1,35 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 import styled from "styled-components"
 import { Container } from "../../elements"
 import { spacing, colors, typo, screenSizes } from "../../utilities"
 import CircuitBG from "../../images/circuit-board.svg"
 
 /** Component */
-const Main = () => (
-  <StyledMain>
-    <Container>
-      <h2>Herzlich Willkommen</h2>
-      <article>
-        <p>
-          Wir unterstützen Sie bei Projekten sowie Migrationen, erarbeiten
-          ganzheitlich Betriebskonzepte und stellen Ihnen zertifizierte Partner
-          zur Verfügung. Selbstverständlich kümmern wir uns auch um die
-          Absicherung der installierten Systeme.
-        </p>
-        <p>
-          Gemeinsam mit Ihnen ermitteln wir Ihre Sicherheitsanforderungen und
-          setzen diese um. Dabei achten wir immer auf den
-          Kosten–Nutzen–Usability-Faktor zwischen Sicherheit und produktivem
-          Arbeiten sowie der Wirtschaftlichkeit von IT-Sicherheit.
-        </p>
-        <p>
-          Für Ihre Mitarbeiter stellen wir bei Bedarf Lösungen für den
-          Fernzugriff auf Ihre Daten, E-Mails und Anwendungen zur Verfügung. Wir
-          entwickeln gemäß Ihren Vorgaben individuelle Softwareanwendungen, die
-          von einer Standard-Software nicht abgedeckt werden.
-        </p>
-        <p>
-          Unser Ziel ist es, die Geschäfts- Prozesse und deren Abläufe unserer
-          Kunden durch moderne, bedarfsgerechte IT-Lösungen und ein
-          umfangreiches Service-Angebot zu unterstützen und für unseren Kunden
-          gewinnbringend zu optimieren.
-        </p>
-      </article>
-    </Container>
-  </StyledMain>
-)
+const Main = () => {
+  const {
+    wordpressPage: { acf: data },
+  } = useStaticQuery(
+    graphql`
+      query Main {
+        wordpressPage(wordpress_id: { eq: 6 }) {
+          acf {
+            uberschrift_hauptext
+            inhalt_haupttext
+          }
+        }
+      }
+    `
+  )
+  return (
+    <StyledMain>
+      <Container>
+        <h2 dangerouslySetInnerHTML={{ __html: data.uberschrift_hauptext }} />
+        <article dangerouslySetInnerHTML={{ __html: data.inhalt_haupttext }} />
+      </Container>
+    </StyledMain>
+  )
+}
 
 /** Styling */
 const StyledMain = styled.main`
