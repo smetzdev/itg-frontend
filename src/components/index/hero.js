16e8ae177337d241d98d7d744ea6596918615e85
Sascha Metz <metz@raum3.de>
2019-05-07 21:00:43 +0200

    Adding StaticQuery to hero.js

--- src/components/index/hero.js
+++ src/components/index/hero.js
@@ -1,23 +1,41 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 import styled from "styled-components"
 import { FlexContainer } from "../../elements"
 import { typo, colors, screenSizes } from "../../utilities"
 import Frank from "../../images/passfoto_cutted.png"
 import BackgroundImage from "../../images/header_background_colored.jpg"
 
+// TODO: Portrait Image and BackgroundImage from Backend
 /** Component */
-const Hero = () => (
-  <StyledHero>
-    <FlexContainer>
-      <h1>
-        IT-Consulting
-        <br />
-        Frank Grossmann
-      </h1>
-      <img src={Frank} alt="Frank Grossmann" />
-    </FlexContainer>
-  </StyledHero>
-)
+const Hero = () => {
+  // Query
+  const {
+    wordpressPage: { acf: data },
+  } = useStaticQuery(
+    graphql`
+      query Hero {
+        wordpressPage(wordpress_id: { eq: 6 }) {
+          acf {
+            uberschrift_hero
+            hintergrundbild
+            portrait
+          }
+        }
+      }
+    `
+  )
+
+  // Render
+  return (
+    <StyledHero>
+      <FlexContainer>
+        <h1 dangerouslySetInnerHTML={{ __html: data.uberschrift_hero }} />
+        <img src={Frank} alt="Frank Grossmann" />
+      </FlexContainer>
+    </StyledHero>
+  )
+}
 
 /** Styling */
 const { weight, size, transform } = typo.headlines.big
