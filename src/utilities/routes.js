60df42c4dfdef53be2d755affa108011a4a56d0d
Sascha Metz <metz@raum3.de>
2019-05-07 21:01:26 +0200

    remove routes from utilities

--- src/utilities/routes.js
+++
@@ -1,4 +0,0 @@
-export const API_URL = "http://hwp-grossmann/wp-json/"
-export const API_FRONTPAGE_URL = `${API_URL}wp/vs/pages/6`
-export const API_IMPRINT_URL = `${API_URL}wp/vs/pages/8`
-export const API_PRIVACY_URL = `${API_URL}wp/vs/pages/10`
