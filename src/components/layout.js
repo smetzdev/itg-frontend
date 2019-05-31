a97acc7026d62cb5115cfecc5225991a913ad447
Sascha Metz <metz@raum3.de>
2019-05-31 13:10:34 +0200

    Adding React Helmet and favicon

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,10 +3,23 @@ import PropTypes from "prop-types"
 import Header from "./header"
 import Footer from "./footer"
 import { GlobalStyle } from "../elements"
+import { Helmet } from "react-helmet"
+import Favicon from "../images/favicon.ico"
 
 /** Component */
 const Layout = ({ children, hasContact }) => (
   <>
+    <Helmet>
+      <title>IT Consulting Frank Großmann</title>
+      <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+      <meta
+        name="description"
+        content="Software, Sicherheit, Administration - von Ihrem IT-Profi"
+      />
+      <link rel="shortcut icon" href={Favicon} />
+    </Helmet>
     <GlobalStyle />
     <Header hasContact={hasContact} />
     {children}
